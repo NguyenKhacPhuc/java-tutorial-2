0f49c1835713c7077f6b3bac2fddfdc3798cb3a4
minh chou <minhchau03032000@gmail.com>
2024-05-09 23:36:59 +0700

    Commit Task3

---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Task3 {
+    static void namnhuan()
+    {
+        int year = 2017;
+        if ((year % 4 == 0) && (year % 100 ==0))
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
