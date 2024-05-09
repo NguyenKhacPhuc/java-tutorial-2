d2310baf7b3d16351a2020c956a48431a080d785
minh chou <minhchau03032000@gmail.com>
2024-05-09 23:34:41 +0700

    Commit Task 2

---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Task2 {
+    static void sumofdigits()
+    {
+        int a = 25;
+        int b = a/10;
+        int c = a-(b*10);
+        int sum = (b+c);
+        System.out.println("số cần tìm là " + sum);
+    }
+}
