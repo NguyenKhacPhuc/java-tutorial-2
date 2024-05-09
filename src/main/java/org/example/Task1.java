a197158fa030d01d1f5788e56c51fa2cdfc0b618
minh chou <minhchau03032000@gmail.com>
2024-05-09 23:33:16 +0700

    Commit Task 1

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Task1 {
+    static void timsonhnhat()
+    {
+        int a = 25;
+        int b = 27;
+        int c = 29;
+        int d;
+        if (a < b)
+            d = a;
+        else d = b;
+        if (d < c)
+            System.out.println ("so nho nhat la = "+ d);
+        else System.out.println ("So nho nhat la " + c);
+    }
+}
